d7301cb3894b8c0fd9095b15022a09c5d131750d
newtoallofthis123 <noobscience123@gmail.com>
2024-11-10 08:54:52 +0530

    chore: PR Update

--- mobile/app/(tabs)/(stuff)/_layout.tsx
+++ mobile/app/(tabs)/(stuff)/_layout.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
 import {useColorScheme} from '@/hooks/useColorScheme';
-import {Stack} from "expo-router";
+// import {Stack} from "expo-router";
+// import {NavigationContainer} from "@react-navigation/native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack"
+import HomeScreen from "@/app/(tabs)/(stuff)/index";
+import CameraScreen from "@/app/(tabs)/(stuff)/camera";
+import CardScreen from "@/app/(tabs)/(stuff)/card";
 
 export default function TabLayout() {
     const colorScheme = useColorScheme();
+    const Stack = createNativeStackNavigator();
 
     return (
-        <Stack screenOptions={{
+        <Stack.Navigator screenOptions={{
             headerShown: false,
         }}>
             <Stack.Screen
@@ -15,13 +21,18 @@ export default function TabLayout() {
                 options={{
                     title: 'Home',
                 }}
+                component={HomeScreen}
             />
             <Stack.Screen
                 name="camera"
                 options={{
                     title: 'Camera',
                 }}
+                component={CameraScreen}
             />
-        </Stack>
+            <Stack.Screen name="card"
+                          component={CardScreen}
+                          options={{title: 'Card'}}/>
+        </Stack.Navigator>
     );
 }
